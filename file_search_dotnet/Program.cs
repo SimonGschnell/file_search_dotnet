195ee0ff4b6b1e5a0c164413b1ae4bedf1e6e58b
Simon Gschnell <simongschnell@gmail.com>
2024-09-29 19:32:24 +0200

    recursive find_file function WIP

---
+++ file_search_dotnet/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+if (args.Length > 1)
+{
+    var file_name = args[0];
+    var dir_name = args[1];
+    DirectoryInfo dir_info = new DirectoryInfo(dir_name);
+    
+    
+    
+    Console.WriteLine(
+        find_file(dir_info, file_name, ref dir_info) switch{
+            true => $"found in {dir_info.Name}, full path: {dir_info.FullName+"/"+file_name}",
+            false => "not found"
+        }
+    );
+    return 0;   
+}
+else
+{
+    Console.WriteLine("Invalid usage of tool, to few arguments");
+    return -1;
+}
+
+
+bool find_file(DirectoryInfo dir, string file_name, ref DirectoryInfo info)
+{
+    foreach(var file in dir.GetFiles())
+    {
+        if(file.Name == file_name)
+        {
+            info = dir;
+            return true;
+        }
+    }
+    foreach(var directory in dir.GetDirectories())
+    {
+        if(find_file(directory,file_name, ref info))
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+
